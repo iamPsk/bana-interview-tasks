a9dfd36b695b96ffc6647b8d69977ba0e9f125f7
webhedo <khonjelwayo@gmail.com>
2021-04-03 10:24:37 +0200

    rm dead code

--- src/app/components/schedule-detail/schedule-detail.component.ts
+++ src/app/components/schedule-detail/schedule-detail.component.ts
@@ -13,69 +13,21 @@ export class ScheduleDetailComponent implements OnInit {
   
   schedule: Schedule;
 
-  elements: any = [
-    {id: 1, first: 'Mark', last: 'Otto', handle: '@mdo'},
-    {id: 2, first: 'Jacob', last: 'Thornton', handle: '@fat'},
-    {id: 3, first: 'Larry', last: 'the Bird', handle: '@twitter'},
-  ];
-
-  headElements = ['Origin station', 'Destination station', 'Cost',];
-  editField: string;
-
-  personList: Array<any> = [
-    { id: 1, name: 'Aurelia Vega', age: 30, companyName: 'Deepends', country: 'Spain', city: 'Madrid' },
-    { id: 2, name: 'Guerra Cortez', age: 45, companyName: 'Insectus', country: 'USA', city: 'San Francisco' },
-    { id: 3, name: 'Guadalupe House', age: 26, companyName: 'Isotronic', country: 'Germany', city: 'Frankfurt am Main' },
-    { id: 4, name: 'Aurelia Vega', age: 30, companyName: 'Deepends', country: 'Spain', city: 'Madrid' },
-    { id: 5, name: 'Elisa Gallagher', age: 31, companyName: 'Portica', country: 'United Kingdom', city: 'London' },
-  ];
-
-  awaitingPersonList: Array<any> = [
-    { id: 6, name: 'George Vega', age: 28, companyName: 'Classical', country: 'Russia', city: 'Moscow' },
-    { id: 7, name: 'Mike Low', age: 22, companyName: 'Lou', country: 'USA', city: 'Los Angeles' },
-    { id: 8, name: 'John Derp', age: 36, companyName: 'Derping', country: 'USA', city: 'Chicago' },
-    { id: 9, name: 'Anastasia John', age: 21, companyName: 'Ajo', country: 'Brazil', city: 'Rio' },
-    { id: 10, name: 'John Maklowicz', age: 36, companyName: 'Mako', country: 'Poland', city: 'Bialystok' },
-  ];
+  headElements = ['Origin station', 'Destination station', 'Cost'];
 
   constructor(private route: ActivatedRoute,
-              private scheduleService: ScheduleService,
-              
+              private scheduleService: ScheduleService,             
               ) { }
 
   ngOnInit(): void {
     this.getSchedule()
-    
+    this.scheduleService.readSchedule(1)
   }
 
   getSchedule():void{
     const id = +this.route.snapshot.paramMap.get('id');
     this.scheduleService.getSchedules().subscribe( schedules => {
       this.schedule = schedules.find( schedule => schedule.id === id)
-
-      console.log(this.schedule);
     })
-  }
-
-  updateList(id: number, property: string, event: any) {
-    const editField = event.target.textContent;
-    this.personList[id][property] = editField;
-  }
-
-  remove(id: any) {
-    this.awaitingPersonList.push(this.personList[id]);
-    this.personList.splice(id, 1);
-  }
-
-  add() {
-    if (this.awaitingPersonList.length > 0) {
-      const person = this.awaitingPersonList[0];
-      this.personList.push(person);
-      this.awaitingPersonList.splice(0, 1);
-    }
-  }
-
-  changeValue(id: number, property: string, event: any) {
-    this.editField = event.target.textContent;
   }
 }
