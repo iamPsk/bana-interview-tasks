37fbedba825342e0c249b2c45fb27af68961229c
webhedo <khonjelwayo@gmail.com>
2021-04-06 14:04:52 +0200

    navbar hack

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,26 +1,46 @@
+import { filter } from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
-import { Router } from "@angular/router"
+import { ActivatedRoute, Router, NavigationStart } from "@angular/router"
+import { Observable } from 'rxjs';
+// import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
 
-  constructor(public router: Router) { }
+export class HeaderComponent implements OnInit {
+  
+  navStart: Observable<NavigationStart>;
+  // nav: HTMLElement = document.getElementsByTagName("nav")[0]
+  
+  constructor(public router: Router, private route: ActivatedRoute) {
+    
+    // Create a new Observable that publishes only the NavigationStart event
+    this.navStart = router.events.pipe(
+      filter(evt => evt instanceof NavigationStart)
+    ) as Observable<NavigationStart>;
+  
+  }
 
   ngOnInit(): void {
-    
-    if(this.router.url !== "/"){
-
-      console.log(
-        'changing color'
-      );
-    }
-    
-    console.log(this.router.url);
-    
+    /****************************************** hack**********************/
+    this.navStart.subscribe( evt => {
+      if(evt.url !== "/"){
+            document.getElementsByTagName("nav")[0].classList.remove('transparent')
+            document.getElementsByTagName("nav")[0].classList.add('top-nav-collapse')
+            document.getElementsByTagName("nav")[0].classList.remove('navbar-dark')
+            document.getElementsByTagName("nav")[0].classList.add('navbar-light')
+          } else {
+            document.getElementsByTagName("nav")[0].classList.add('navbar-dark')
+            document.getElementsByTagName("nav")[0].classList.remove('navbar-light')
+            
+            document.getElementsByTagName("nav")[0].classList.add('transparent')
+            document.getElementsByTagName("nav")[0].classList.remove('top-nav-collapse')
+          }
+        })
+    /****************************************** hack**********************/
   }
 
 }
